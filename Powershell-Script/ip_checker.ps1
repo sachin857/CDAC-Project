4c9fb5afcd0a92d65591150e61c95cdfffa502e7
Sachin Uikey <conanxlr8@gmail.com>
2024-09-02 13:26:42 +0530

    Update 4.1

--- Powershell-Script/ip_checker.ps1
+++ Powershell-Script/ip_checker.ps1
@@ -1,4 +1,25 @@
 
+wsl.exe -e exit
+Start-Sleep -Seconds 5
+$ips = (wsl hostname -I).Trim().split()
+$ip = $ips[0]
+netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=11434
+netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=22
+
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=11434 connectaddress=$ip connectport=11434
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=$ip connectport=22
+
+
+# to check port forwarding
+# netsh interface portproxy show all
+
+
+
+
+---------------------------------------
+
+
+
 wsl.exe -e exit  # Restart the WSL2 instance
 Start-Sleep -Seconds 5  # Wait for 5 seconds to ensure WSL2 has exited
 
