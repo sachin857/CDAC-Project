d320082b0e12914005e883d81b05934039559135
Sachin Uikey <conanxlr8@gmail.com>
2024-09-02 13:25:58 +0530

    Update 4.0

--- Master-Server/script-down.sh
+++ Master-Server/script-down.sh
@@ -1,6 +1,69 @@
 
 #!/bin/bash
 
+CUSTOM_SCRIPT="node-down.sh"
+#ACTIVE_NODE_FILE="/root/project-2/db/active_node.csv"
+> "/root/project-2/db/temp_node.csv"
+
+check_gpu_difference() {
+
+
+if [ -s "/root/project-2/db/active_node.csv" ]; then
+    
+    while read entry; do
+	
+	    MAIN_SERVER=$(echo "$entry" | cut -d',' -f1)
+	    last_timestamp=$(echo "$entry" | cut -d',' -f2)
+	    last_utilization=$(echo "$entry" | cut -d',' -f3 | tr -d '%')
+	
+	    last_timestamp_sec=$(date -d "$last_timestamp" +%s)
+	    current_time_sec=$(date +%s)
+	    time_diff=$((current_time_sec - last_timestamp_sec))
+
+	    
+	    
+	    current_utilization=$(cat /root/project-2/db/a_metric)
+	    
+	    utilization_diff=$((last_utilization - current_utilization))
+
+	    #echo "Time difference: $time_diff seconds"
+	    #echo "Utilization difference: $utilization_diff%"
+
+	    
+	    if [ "${utilization_diff#-}" -lt 60 ]; then
+	        
+	        ssh -t $MAIN_SERVER $CUSTOM_SCRIPT
+	       	       
+	    else
+	    	echo "$entry" >> "/root/project-2/db/temp_node.csv"
+	    fi
+    
+    done < "/root/project-2/db/active_node.csv"
+fi
+
+
+if [ -s "/root/project-2/db/temp_node.csv" ]; then
+
+    cat "/root/project-2/db/temp_node.csv" > "/root/project-2/db/active_node.csv"
+
+fi
+
+}
+
+
+while true; do
+    check_gpu_utilization
+    sleep 120
+done
+
+
+----------------------------------------------------
+
+
+
+
+#!/bin/bash
+
 CUSTOM_SCRIPT="node-down.sh"  # The script to run on worker nodes to stop Docker containers
 > "/root/project-2/db/temp_node.csv"  # Clear the temporary node file
 
@@ -27,7 +90,7 @@ check_gpu_difference() {
             utilization_diff=$((last_utilization - current_utilization))  # Calculate GPU utilization difference
         
             # Check if the utilization difference is less than 60%
-            if [ "${utilization_diff}" -lt 60 ]; then
+            if [ "${utilization_diff#-}" -lt 60 ]; then
                 ssh -t $MAIN_SERVER $CUSTOM_SCRIPT  # SSH into the worker node and run the scale-down script
             else
                 echo "$entry" >> "/root/project-2/db/temp_node.csv"  # Keep the node active by storing in temp file
--- Master-Server/script-up.sh
+++ Master-Server/script-up.sh
@@ -1,3 +1,47 @@
+#!/bin/bash
+
+
+CUSTOM_SCRIPT="node-up.sh"
+THRESHOLD_HIGH=70
+INTERVAL=2
+
+
+ACTIVE_NODE_FILE="/root/project-2/db/active_node.csv"
+
+
+check_gpu_utilization() {
+    utilization=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}')
+    echo "Current GPU utilization: $utilization%"
+
+    if [ "$utilization" -gt "$THRESHOLD_HIGH" ]; then
+        echo "GPU utilization above $THRESHOLD_HIGH%. Sending alert to main server."
+        
+
+       while read ip;do
+
+			if grep -q $ip "/root/project-2/db/active_node.csv";then
+				
+				continue
+				echo "found"
+				
+			else
+				timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+		      
+		        echo "$ip,$timestamp,$utilization%" >> "$ACTIVE_NODE_FILE"
+				ssh -t $ip $CUSTOM_SCRIPT
+			fi	
+
+		done < "/root/project-2/db/workers"
+    fi
+}
+
+
+while true; do
+    check_gpu_utilization
+    sleep $INTERVAL
+done
+-------------------------------------
+
 
 #!/bin/bash
 
