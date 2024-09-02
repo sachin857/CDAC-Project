c0c26c8d4d403e6fbe8991ec5ef74f65a2a917b5
Sachin Uikey <conanxlr8@gmail.com>
2024-09-02 13:44:22 +0530

    Update 4.4

--- Worker-Server/node-up.sh
+++ Worker-Server/node-up.sh
@@ -1,9 +1,6 @@
 
-
-
 #!/bin/bash
 
-# Run Ollama container with GPU support
 docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
 
--- Worker-Server/syncdb.sh
+++ Worker-Server/syncdb.sh
@@ -1,30 +1,16 @@
-#!/bin/bash
-
-#echo $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}') > /root/project-2/db/a_metric
-#rsync /root/project-2/db/* 192.168.82.164:/root/project-2/db/
-#echo "1" >> /root/project-2/db/test
-
-while true; do
-    echo $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}') > /root/project-2/db/a_metric
-    rsync /root/project-2/db/* 192.168.82.164:/root/project-2/db/
-    sleep 120
-done
-
-----------------------------
-
 
 #!/bin/bash
 
-# Continuously monitor GPU utilization and sync the data with the master node
+
 while true; do
 
-    # Capture the current GPU utilization percentage and save it to the a_metric file
+ 
     echo $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}') > /root/project-2/db/a_metric
 
-    # Sync the GPU metric and other data from the worker node to the master node using rsync
+ # make sure to add master ip below
     rsync /root/project-2/db/* <master-ip>:/root/project-2/db/
 
-    # Wait for 120 seconds before repeating the process
+    
     sleep 120
 done
 
